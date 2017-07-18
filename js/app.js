f01572ebbe4f8e0e7ae20bba8822c3d4634134c7
cocacolabe <cocacolabear.la@gmail.com>
2017-07-17 22:12:08 -0400

    passing name of the location to list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
+var initialLocation = [
+    {
+        name: 'Bronx Zoo',
+        latlng: {lat: 40.850595, lng: -73.876998},
+    },
 
+    {
+        name: 'Central Park Zoo',
+        latlng: {lat: 40.767778, lng: -73.971834},
+    },
 
+    {
+        name: 'Brooklyn Zoo NY',
+        latlng: {lat: 40.711663, lng: -73.935463},
+    },
 
+        {
+        name: 'Queens Zoo',
+        latlng: {lat: 40.744434, lng: -73.849741},
+    },
+
+        {
+        name: 'Staten Island Zoo',
+        latlng: {lat: 40.625124, lng: -74.115370},
+    }
+]
+
+var Location = function(data){
+    this.name = ko.observable(data.name);
+}
+
+var ViewModel = function(){
+    var self = this;
+    this.locationList = ko.observableArray([]);
+    initialLocation.forEach(function(listItem){
+        self.locationList.push( new Location(listItem) );
+    });
+    
+    this.currentLocation = ko.observable( this.locationList()[0]);
+    
+}
 
       function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
@@ -14,3 +52,4 @@
         });
       }
   
+ko.applyBindings(new ViewModel());
